83b46f266a4e1bcbc27dcef9fb2e058f4892a1e4
Till Krischer <till@krischer.io>
2021-08-27 21:33:55 +0200

    env

--- price-be/src/server.ts
+++ price-be/src/server.ts
@@ -3,6 +3,8 @@ import express from 'express';
 export const start = () => {
   const app = express();
 
+  app.use('/', express.static('public'));
+
   app.get('/api', (req, res) => {
     res.send('Hello World!');
   });
