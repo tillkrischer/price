5230187e70f67aead3fc0346a22b0a97c7e0430b
Till Krischer <till@krischer.io>
2021-08-27 18:56:01 +0200

    be config

--- price-be/src/index.ts
+++ price-be/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
